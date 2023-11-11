6c1555a793caae55e1a757650aa9492461ae6f5d
FosterClark48 <fosterclark48@yahoo.com>
2023-11-11 17:10:58 -0600

    Added complete schema to use instead of just the Task object type

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
-const { TaskType } = require('./schema/schema.js');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
-  schema: TaskType,
+  schema,
   graphiql: true,
 }));
 
