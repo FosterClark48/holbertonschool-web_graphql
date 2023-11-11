fe7da447ea1ef6edc1408ec77d448616653b7100
FosterClark48 <fosterclark48@yahoo.com>
2023-11-11 17:10:31 -0600

    Added RootQuery object type that serves as the entry point for GraphQL queries. exported compelte schema

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require("graphql");
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -10,4 +10,17 @@ const TaskType = new GraphQLObjectType({
   }),
 });
 
-module.exports = { TaskType };
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
