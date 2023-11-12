9522543e51805f197f3a9268dbf90ede321e824e
FosterClark48 <fosterclark48@yahoo.com>
2023-11-12 14:48:34 -0600

    Updated to establish a connection to my MongoDB database using Mongoose

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.use('/graphql',graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+mongoose.connect('mongodb+srv://5538:Vlcoh0XaRl92YtYF@graphqlcluster.qi4ingp.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
